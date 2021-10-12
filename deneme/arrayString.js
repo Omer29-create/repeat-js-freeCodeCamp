02b1f192bc43da62197cdcf8e12412ab2b4a3b69
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-12 11:05:45 +0200

    Update arrayString.js

--- deneme/arrayString.js
+++ deneme/arrayString.js
@@ -179,3 +179,16 @@ const fruits = {
                                                       b
                                                       c
                                                       d */
+
+
+//  Eleman degistirme ve ekleme
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
+
+// Display the Array:
+document.getElementById("demo").innerHTML = cars; // {Toyota, Volvo, BMW, Audi}
