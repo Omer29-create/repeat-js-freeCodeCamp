ecf47807a23394992cff8710f886a6bfa0087378
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-12 10:52:18 +0200

    Update arrayString.js

--- deneme/arrayString.js
+++ deneme/arrayString.js
@@ -1,3 +1,4 @@
+// random siralama
 const points = [40, 100, 1, 5, 25, 10];
 document.getElementById("demo").innerHTML = points;  
 
@@ -9,4 +10,20 @@ function myFunction() {
     points[j] = k
   }
   document.getElementById("demo").innerHTML = points;
+}
+
+
+// max 
+const points = [40, 100, 1, 5, 25, 10];
+document.getElementById("demo").innerHTML = myArrayMax(points);
+
+function myArrayMax(arr) {
+  let len = arr.length;
+  let max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
 }
