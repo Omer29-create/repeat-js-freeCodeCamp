19d2ba45f6497dec6a1b71f2628008a21a6eeab3
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-16 17:37:56 +0200

    Update functions.js

--- MathVeSonrasi/functions.js
+++ MathVeSonrasi/functions.js
@@ -32,3 +32,16 @@ function myFunction() {
   return this;
 }
 document.getElementById("demo").innerHTML = x;  // [object Window]
+
+
+// This is a function constructor:
+function myFunction(arg1, arg2) {
+  this.firstName = arg1;
+  this.lastName  = arg2;
+}
+
+// This creates a new object
+const myObj = new myFunction("John", "Doe");
+
+// This will return "John"
+myObj.firstName;
