9fe5926acc087470c4452cbb348608abf6b54131
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-16 17:26:33 +0200

    Update functions.js

--- MathVeSonrasi/functions.js
+++ MathVeSonrasi/functions.js
@@ -1,3 +1,9 @@
 (function () {
   document.getElementById("demo").innerHTML = "Hello! I called myself";
 })();   // Hello! I called myself
+
+
+function myFunction(a, b) {
+  return arguments.length;
+}
+document.getElementById("demo").innerHTML = myFunction(4, 3);  // 2
