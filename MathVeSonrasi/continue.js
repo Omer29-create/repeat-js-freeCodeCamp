f936b3e971886297db333a24e6c53ef67f140e12
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-16 15:22:07 +0200

    Update continue.js

--- MathVeSonrasi/continue.js
+++ MathVeSonrasi/continue.js
@@ -22,3 +22,15 @@ window.addEventListener("load", hello);
 
 //A button object calls the function:
 document.getElementById("btn").addEventListener("click", hello); //[object Window]
+
+
+class Car {
+    constructor(name, year) { // Constructor olusturma
+      this.name = name;
+      this.year = year;
+    }
+  }
+  
+  const myCar = new Car("Ford", 2014);
+  document.getElementById("demo").innerHTML =
+  myCar.name + " " + myCar.year; // Ford 2014
