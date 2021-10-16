4022c5d5bd6ae9b548930d6790969a1fabf5c78f
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-16 15:35:07 +0200

    Update versions.js

--- MathVeSonrasi/versions.js
+++ MathVeSonrasi/versions.js
@@ -17,3 +17,20 @@ function myFunction(value) {
 const txt = '{"name":"John", "age":30, "city":"New York"}'
 const obj = JSON.parse(txt);
 document.getElementById("demo").innerHTML = obj.name + ", " + obj.age; // John, 30
+
+
+var person = {
+    firstName: "John",
+    lastName : "Doe",
+    language : "NO",
+    get lang() {
+      return this.language;
+    },
+    set lang(value) {
+      this.language = value;
+    }
+  };
+  // Set an object property using a setter:
+  person.lang = "en";
+  // Display data from the object using a getter:
+  document.getElementById("demo").innerHTML = person.lang; // en
