469de717b02ddeffeadb617f356fe022fb89c8dc
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-16 18:01:22 +0200

    Update class.js

--- MathVeSonrasi/class.js
+++ MathVeSonrasi/class.js
@@ -20,3 +20,23 @@ class Model extends Car {
 
 let myCar = new Model("Ford", "Mustang");
 document.getElementById("demo").innerHTML = myCar.show();
+
+
+// static key
+class Car {
+  constructor(name) {
+    this.name = name;
+  }
+  static hello() {
+    return "Hello!!";
+  }
+}
+
+let myCar = new Car("Ford");
+
+// You can calll 'hello()' on the Car Class:
+document.getElementById("demo").innerHTML = Car.hello();
+
+// But NOT on a Car Object:
+// document.getElementById("demo").innerHTML = myCar.hello();
+// this will raise an error.
