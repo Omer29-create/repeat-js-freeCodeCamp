3b491bb83f6d8235a7db47778e66fe473528f3c8
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-16 17:12:24 +0200

    Create objekten.js

---
+++ MathVeSonrasi/objekten.js
@@ -0,0 +1,18 @@
+// Home Made Iterable
+function myNumbers() {
+  let n = 0;
+  return {
+    next: function() {
+      n += 10;
+      return {value:n, done:false};
+    }
+  };
+}
+
+// Create Iterable
+const n = myNumbers();
+n.next(); // 10
+n.next(); // 20
+n.next(); // 30
+
+document.getElementById("demo").innerHTML = n.next().value;
