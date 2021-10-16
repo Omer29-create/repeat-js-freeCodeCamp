372e2e3eecac66aa356491789d78ab6966ec8bf2
Ömer Aydin <64536242+Omer29-create@users.noreply.github.com>
2021-10-16 15:25:09 +0200

    Update continue.js

--- MathVeSonrasi/continue.js
+++ MathVeSonrasi/continue.js
@@ -57,3 +57,12 @@ obj.employees[1].firstName + " " + obj.employees[1].lastName;
 let x = 15 * 5;
 debugger;  // Burada duracak ve 3. satira gitmeyecek
 document.getElementById("demo").innerHTML = x; 
+
+
+let x1 = "";             // new primitive string
+let x2 = 0;              // new primitive number
+let x3 = false;          // new primitive boolean
+const x4 = {};           // new object
+const x5 = [];           // new array object
+const x6 = /()/;         // new regexp object
+const x7 = function(){}; // new function object
